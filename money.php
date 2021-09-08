cade0a65a43919657f6cba103fd7dfb5e148b661
Mariya Makarenko <89764563+MariMak8@users.noreply.github.com>
2021-09-08 16:20:43 +0300

    Add files via upload

---
+++ money.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace mariya\src;
+
+class money
+{
+    private $amount;
+    private  \mariya\src\currency $currency;
+
+    public function __construct ($amount,\mariya\src\currency $currency){
+        $this->setAmount($amount);
+        $this->setCurrency($currency);
+    }
+
+
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+
+    public function setAmount(float $amount): void
+    {
+        $this->amount = $amount;
+    }
+
+
+    public function getCurrency(): currency
+    {
+        return $this->currency;
+    }
+
+
+
+    public function setCurrency(currency $currency): void
+    {
+        $this->currency = $currency;
+    }
+    public function equal (money  $code){
+
+        if($this == $code){
+            return true;
+        }
+        return false;
+    }
+    public function add (money $code){
+
+        if(!$this->currency->equal( $code->currency)){
+            throw new \InvalidArgumentException('Different currences cant be add');
+        }
+
+        return new money($this->getAmount()+$code->getAmount(), $this->getCurrency());
+    }
+}
